587b6b27642b19c33d7f74a8c9aa00074cc7537c
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-01-08 16:03:16 +0300

    add: section experts

--- about.php
+++ about.php
@@ -13,7 +13,25 @@ include_once("templateParts/head.php"); ?>
 	include_once("templateParts/navbar.php"); ?>
 	<?php include_once("templateParts/header-secondary.php"); ?>
 
+	<section class="section-experts">
+		<div class="container">
+			<div class="experts-wrapper">
+				<picture class="experts-photo">
+					<source type="image/webp" srcset="img/experts.webp">
+					<source type="image/jpeg" srcset="img/experts.jpg">
+					<img src="img/experts.jpg" class="experts-photo" alt="experts">
+				</picture>
+				<div class="experts-content">
+					<div class="separator"></div>
+					<h2 class="section-title">мы - эксперты в области производства химии</h2>
+					<p class="experts-text">
+						Разнообразный и богатый опыт говорит нам, что перспективное планирование однозначно фиксирует необходимость поставленных обществом задач! Есть над чем задуматься: ключевые особенности структуры проекта могут быть объединены в целые кластеры себе подобных. Интерактивные прототипы, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут описаны максимально подробно. Значимость этих проблем настолько очевидна, что перспективное планирование играет важную роль в формировании системы массового участия.
+					</p>
+				</div>
+			</div>
 
+		</div>
+	</section>
 
 	<?php include_once('templateParts/footer.php'); ?>
 	<?php include_once('templateParts/scripts.php'); ?>
